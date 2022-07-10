efcabd1f88ab1f0643ed110c49f641cb83be6eb7
Svancik <thesvancik@gmail.com>
2022-07-10 21:49:30 +0200

    reusable chart

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,27 @@
+import "./chart.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+export default function Chart({ title, data, dataKey, grid }) {
+  return (
+    <div className="chart">
+      <h3 className="chartTitle">{title}</h3>
+      {/* aspect={4/1} nám níže říká že pokud je např. width 400px tak height bude 100px  */}
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart data={data}>
+          <XAxis dataKey="name" stroke="#5550bd" />
+          <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
+          {/* Pomocí Tooltip níže jsme schopni při hoveru zobrazit data */}
+          <Tooltip />
+          {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
