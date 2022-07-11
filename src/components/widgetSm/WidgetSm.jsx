e3babcf94f1a312887cfa5174886955bf3f398f9
Svancik <thesvancik@gmail.com>
2022-07-11 20:14:18 +0200

    user information components

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,90 @@
+import "./widgetSm.css";
+import { Visibility } from "@mui/icons-material";
+export default function WidgetSm() {
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItem">
+          <img
+            src="https://akns-images.eonline.com/eol_images/Entire_Site/2016124/rs_600x600-160224090109-600.bale.cm.22416.jpg?fit=around%7C1200:1200&output-quality=90&crop=1200:1200;center,top"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Patrick Bateman</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+
+        <li className="widgetSmListItem">
+          <img
+            src="https://akns-images.eonline.com/eol_images/Entire_Site/2016124/rs_600x600-160224090109-600.bale.cm.22416.jpg?fit=around%7C1200:1200&output-quality=90&crop=1200:1200;center,top"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Patrick Bateman</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+
+        <li className="widgetSmListItem">
+          <img
+            src="https://akns-images.eonline.com/eol_images/Entire_Site/2016124/rs_600x600-160224090109-600.bale.cm.22416.jpg?fit=around%7C1200:1200&output-quality=90&crop=1200:1200;center,top"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Patrick Bateman</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+
+        <li className="widgetSmListItem">
+          <img
+            src="https://akns-images.eonline.com/eol_images/Entire_Site/2016124/rs_600x600-160224090109-600.bale.cm.22416.jpg?fit=around%7C1200:1200&output-quality=90&crop=1200:1200;center,top"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Patrick Bateman</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+
+        <li className="widgetSmListItem">
+          <img
+            src="https://akns-images.eonline.com/eol_images/Entire_Site/2016124/rs_600x600-160224090109-600.bale.cm.22416.jpg?fit=around%7C1200:1200&output-quality=90&crop=1200:1200;center,top"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Patrick Bateman</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
