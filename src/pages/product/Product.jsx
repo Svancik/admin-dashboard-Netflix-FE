e27db1306d16cce813a2fb8cb609aa14e4c88c8d
Svancik <thesvancik@gmail.com>
2022-10-01 07:20:00 +0200

    new movie - text input

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -10,7 +10,7 @@ export default function Product() {
     <div className="product">
       <div className="productTitleContainer">
         <h1 className="productTitle">Movie</h1>
-        <Link to="/newproduct">
+        <Link to="/newMovie">
           <button className="productAddButton">Create</button>
         </Link>
       </div>
